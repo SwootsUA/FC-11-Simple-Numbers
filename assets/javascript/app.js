a395f4b24023ff75269ae7c03e78574791d07f13
Kostiantyn <kostya20022210@gmail.com>
2025-01-22 08:56:22 +0200

    Implement prime number checker

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+for (let i = 0; i < 100; i++) {
+    if (isPrime(i)) {
+        console.log('Number', i, 'is Simple');
+    }
+}
+
+function isPrime(number) {
+    if (number <= 1) {
+        return false;
+    }
+
+    for (let i = number - 1; i > 1; i--) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
